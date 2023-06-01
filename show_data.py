49c3d557c053449813fd7bebf26af73f7447dd0d
DianaRatnikova <Diana_r@bk.ru>
2023-06-02 01:09:36 +0300

    Revert "Вытаскиваю N топ-постов, формирую ссылки на комменты к ним"
    This reverts commit de850ffbffc3807a9b31d80552a699a10d7e6a39.

--- show_data.py
+++
@@ -1,5 +0,0 @@
-import requests
-
-def show_all_posts(result_subreddit: requests.models.Response, limit: int) -> None:
-    for num_of_post in range(limit):
-        print(f"{result_subreddit.json()['data']['children'][num_of_post]['data']['title'] = } \n")
